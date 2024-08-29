6eb1e9a8715fe7e5af1944d8223a5abfa1afded2
utsavpatel562 <utsavpatel562@gmail.com>
2024-08-29 12:09:12 -0400

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,33 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./home";
+import Contact from "./contact";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
 ]);
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl="/"
+    ></ClerkProvider>
     <RouterProvider router={router} />
+    <ClerkProvider />
   </StrictMode>
 );
