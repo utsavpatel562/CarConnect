80761f4b90c33cf9b90ffe159db5b8f60d16c8a9
utsavpatel562 <utsavpatel562@gmail.com>
2024-09-01 20:32:47 -0400

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
+import { UserButton, useUser } from "@clerk/clerk-react";
 import React from "react";
+import { Button } from "./ui/button";
 
 function Header() {
+  const { user, isSignedIn } = useUser();
   return (
     <>
-      <div>
-        <img src="/logo4.png" alt="Logo" width={150} height={150} />
+      <div className="flex justify-between items-center shadow-sm p-5">
+        <img src="/logo4.png" alt="Logo" width={150} height={100} />
+        <ul className="hidden md:flex gap-16">
+          <li>Home</li>
+          <li>Search</li>
+          <li>New</li>
+          <li>Preowned</li>
+        </ul>
+        {isSignedIn ? (
+          <div>
+            <UserButton />
+            <Button>Submit Listing</Button>
+          </div>
+        ) : (
+          <div>Submit Listing</div>
+        )}
       </div>
     </>
   );
