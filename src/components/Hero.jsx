e837a5291cf0e2d5261b8167972b024d602f3314
utsavpatel562 <utsavpatel562@gmail.com>
2024-09-01 22:18:15 -0400

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Search from "./Search";
 
 function Hero() {
   return (
@@ -7,6 +8,7 @@ function Hero() {
         <div className="flex flex-col items-center p-10 py-20 gap-2 h-[600px] w-full bg-[#eef0fc]">
           <h2 className="text-lg">Find cars for sales and for rent near you</h2>
           <h2 className="text-[60px] font-bold">Find your dream car</h2>
+          <Search />
         </div>
       </div>
     </>
---
+++ src/components/Search.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+function Search() {
+  return (
+    <>
+      <div className="p-2 bg-white rounded-md md:rounded-full flex-col md:flex md:flex-row gap-10 px-5 items-center w-full md:w-max">
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+    </>
+  );
+}
+
+export default Search;
