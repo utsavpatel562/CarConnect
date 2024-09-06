c87b5af48f25e663dcb2ff7f96ebd9f14ac9277c
utsavpatel562 <utsavpatel562@gmail.com>
2024-09-06 13:22:00 -0400

    Add Listing Sections

--- src/add-listing/index.jsx
+++ src/add-listing/index.jsx
@@ -1,9 +1,20 @@
+import Header from "@/components/Header";
 import React from "react";
 
 function AddListing() {
   return (
     <>
-      <div>Add Listing</div>
+      <div>
+        <Header />
+        <div className="px-10 md:px-20 my-10">
+          <h2 className="font-bold text-4xl">Add New Listing</h2>
+          <form className="p-10 border-2 rounded-xl mt-10">
+            <div>
+              <h2 className="font-medium text-xl mb-6"> Car Details</h2>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
