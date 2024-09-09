dad287f75731c1148e511be632276318ba08c4d4
utsavpatel562 <utsavpatel562@gmail.com>
2024-09-09 10:21:13 -0400

    New Commit

--- src/aboutme/index.jsx
+++ src/aboutme/index.jsx
@@ -1,11 +1,56 @@
 import Header from "@/components/Header";
 import React from "react";
+import { FaInstagram } from "react-icons/fa6";
+import { FiGithub } from "react-icons/fi";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function AboutUs() {
   return (
     <>
       <div>
         <Header />
+        <div className="flex justify-center items-center my-20">
+          <div className="w-[30%]">
+            <article className="group">
+              <img
+                alt=""
+                src="/mypic1.jpeg"
+                className="h-52 w-full rounded-xl object-cover shadow-xl transition"
+              />
+
+              <div className="p-4">
+                <a href="#">
+                  <h3 className="text-lg font-medium text-gray-900">
+                    About Me
+                  </h3>
+                </a>
+
+                <p className="mt-2 text-justify text-sm/relaxed text-gray-500">
+                  I am Utsav Patel, the developer behind CarConnect, a
+                  marketplace platform for car buyers and sellers. Built with
+                  React, Tailwind CSS, Drizzle ORM, and real-time chat powered
+                  by Sendbird, the app features a sleek UI using ShadCN,
+                  Acernity, and Hyper UI. Deployed on Netlify, CarConnect
+                  delivers a fast and responsive user experience.
+                </p>
+                <div className="my-5 flex gap-3">
+                  <FaInstagram
+                    style={{ fontSize: "32px" }}
+                    className="cursor-pointer hover:bg-pink-500 hover:text-white p-1 rounded-lg"
+                  />
+                  <FiGithub
+                    style={{ fontSize: "32px" }}
+                    className="cursor-pointer hover:bg-slate-900 hover:text-white p-1 rounded-lg"
+                  />
+                  <FaLinkedinIn
+                    style={{ fontSize: "32px" }}
+                    className="cursor-pointer hover:bg-blue-600 hover:text-white p-1 rounded-lg"
+                  />
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
       </div>
     </>
   );
