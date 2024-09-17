98ef5070ced4eaf286216a09e823e63c821faa52
utsavpatel562 <utsavpatel562@gmail.com>
2024-09-16 21:09:10 -0400

    New Commit

--- src/add-listing/components/UploadImage.jsx
+++ src/add-listing/components/UploadImage.jsx
@@ -4,17 +4,20 @@ function UploadImage() {
   return (
     <>
       <div>
-        <div>
+        <h2 className="text-xl my-4 font-bold text-slate-700">
+          Upload Car Images
+        </h2>
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-1">
           <label htmlFor="upload-images">
-            <div className="border rounded-xl border-dotted border-primary bg-green-100 p-4">
-              <h2 className="text-lg">+</h2>
+            <div className="border rounded-xl border-dotted border-primary bg-green-100 p-10 cursor-pointer hover:shadow-md">
+              <h2 className="text-2xl text-center text-primary">+</h2>
             </div>
           </label>
           <input
             type="file"
             multiple={true}
             id="upload-images"
-            className="opacity"
+            className="opacity-0"
           />
         </div>
       </div>
