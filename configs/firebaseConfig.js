cd4ff4dda716298722e6725f4e0f494c205c226e
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-09-19 15:38:46 -0400

    Update firebaseConfig.js

--- configs/firebaseConfig.js
+++ configs/firebaseConfig.js
@@ -7,9 +7,9 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  authDomain: "carconnect-d23fd.firebaseapp.com",
+  projectId: "carconnect-d23fd",
+  storageBucket: "carconnect-d23fd.appspot.com",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
