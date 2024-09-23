17a0dc3915d91c0fa58d03235e3f7670076095e4
utsavpatel562 <utsavpatel562@gmail.com>
2024-09-23 19:25:56 -0400

    New Commit

--- src/profile/index.jsx
+++ src/profile/index.jsx
@@ -1,19 +1,37 @@
 import Header from "@/components/Header";
-import React from "react";
+import React, { useEffect } from "react";
 import MyListing from "./components/MyListing";
 import { TbListDetails } from "react-icons/tb";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { MdAllInbox } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
+import { db } from "./../../configs";
+import { CarImages, CarListing } from "./../../configs/schema";
+import { desc, eq } from "drizzle-orm";
+import { useUser } from "@clerk/clerk-react";
 
 function Profile() {
+  const { user } = useUser();
+  useEffect(() => {
+    user && GetUserCarListing();
+  }, [user]);
+  const GetUserCarListing = async () => {
+    const result = await db
+      .select()
+      .from(CarListing)
+      .leftJoin(CarImages, eq(CarListing.id, CarImages.carListingId))
+      .where(eq(CarListing.createdBy, user?.primaryEmailAddress.emailAddress))
+      .orderBy(desc(CarListing.id));
+    console.log(result);
+  };
+
   return (
     <>
       <div>
         <Header />
-        <div className="px-10 md:px-20 my-10 mb-20">
-          <Tabs defaultValue="account" className="w-[400px]">
-            <TabsList>
+        <div className="px-24 md:px-24 my-10 mb-20">
+          <Tabs defaultValue="my-listing" className="w-full">
+            <TabsList className="w-full flex justify-start">
               <TabsTrigger
                 value="my-listing"
                 className="flex items-center gap-2"
@@ -21,21 +39,21 @@ function Profile() {
                 <TbListDetails />
                 <span>My Listing</span>
               </TabsTrigger>
-              <TabsTrigger value="inbox">
+              <TabsTrigger value="inbox" className="flex items-center gap-2">
                 <MdAllInbox />
                 Inbox
               </TabsTrigger>
-              <TabsTrigger value="profile">
+              <TabsTrigger value="profile" className="flex items-center gap-2">
                 <CgProfile />
                 Profile
               </TabsTrigger>
             </TabsList>
-            <TabsContent value="my-listing">For my listing.</TabsContent>
+            <TabsContent value="my-listing" className="my-5">
+              <MyListing />
+            </TabsContent>
             <TabsContent value="inbox">inbox</TabsContent>
             <TabsContent value="profile">profile</TabsContent>
           </Tabs>
-
-          <MyListing />
         </div>
       </div>
     </>
