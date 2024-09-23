fb925122149c436f254335afaf9a83fd85081afc
utsavpatel562 <utsavpatel562@gmail.com>
2024-09-23 18:51:26 -0400

    New Commit

--- src/profile/index.jsx
+++ src/profile/index.jsx
@@ -1,25 +1,38 @@
 import Header from "@/components/Header";
 import React from "react";
 import MyListing from "./components/MyListing";
+import { TbListDetails } from "react-icons/tb";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { MdAllInbox } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
 
 function Profile() {
   return (
     <>
       <div>
         <Header />
-        <div className="px-10 md:px-20 my-10">
+        <div className="px-10 md:px-20 my-10 mb-20">
           <Tabs defaultValue="account" className="w-[400px]">
             <TabsList>
-              <TabsTrigger value="account">Account</TabsTrigger>
-              <TabsTrigger value="password">Password</TabsTrigger>
+              <TabsTrigger
+                value="my-listing"
+                className="flex items-center gap-2"
+              >
+                <TbListDetails />
+                <span>My Listing</span>
+              </TabsTrigger>
+              <TabsTrigger value="inbox">
+                <MdAllInbox />
+                Inbox
+              </TabsTrigger>
+              <TabsTrigger value="profile">
+                <CgProfile />
+                Profile
+              </TabsTrigger>
             </TabsList>
-            <TabsContent value="account">
-              Make changes to your account here.
-            </TabsContent>
-            <TabsContent value="password">
-              Change your password here.
-            </TabsContent>
+            <TabsContent value="my-listing">For my listing.</TabsContent>
+            <TabsContent value="inbox">inbox</TabsContent>
+            <TabsContent value="profile">profile</TabsContent>
           </Tabs>
 
           <MyListing />
