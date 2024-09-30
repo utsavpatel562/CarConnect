938e5136b5cb287720fcedc5890642eb478bcf54
utsavpatel562 <utsavpatel562@gmail.com>
2024-09-30 13:07:48 -0400

    New Updated Code

--- src/home.jsx
+++ src/home.jsx
@@ -6,6 +6,7 @@ import MostSearchedCar from "./components/MostSearchedCar";
 
 import InfoSection from "./components/InfoSection";
 import Footer from "./components/Footer";
+import HowitWorks from "./components/HowitWorks";
 
 function Home() {
   return (
@@ -15,6 +16,7 @@ function Home() {
       <Catetory />
       <MostSearchedCar />
       <InfoSection />
+      <HowitWorks />
       <Footer />
     </div>
   );
