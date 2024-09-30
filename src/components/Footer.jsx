736c4c1afe4eb289d990ec5dcb41f5f68b4d3842
utsavpatel562 <utsavpatel562@gmail.com>
2024-09-30 12:53:10 -0400

    New Updated Code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
             <img src="/logo2.png" width={40} />
           </div>
 
-          <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500">
+          <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-400">
             © 2024 CarConnect. All rights reserved. Connecting you with the
             perfect car, anytime, anywhere.
           </p>
@@ -17,33 +17,31 @@ function Footer() {
           <ul className="mt-12 flex justify-center gap-6 md:gap-8">
             <li>
               <a
-                href="#"
+                href="https://www.linkedin.com/in/utsav-patel-coder"
                 rel="noreferrer"
                 target="_blank"
-                className="text-gray-700 transition hover:text-gray-700/75"
+                className="text-gray-400 transition hover:text-gray-400/75"
               >
-                <span className="sr-only">Facebook</span>
+                <span className="sr-only">LinkedIn</span>
                 <svg
-                  className="size-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="22"
+                  height="22"
                   fill="currentColor"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
+                  class="bi bi-linkedin"
+                  viewBox="0 0 16 16"
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                    clipRule="evenodd"
-                  />
+                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                 </svg>
               </a>
             </li>
 
             <li>
               <a
-                href="#"
+                href="https://www.instagram.com/utsavpatel_01/"
                 rel="noreferrer"
                 target="_blank"
-                className="text-gray-700 transition hover:text-gray-700/75"
+                className="text-gray-400 transition hover:text-gray-400/75"
               >
                 <span className="sr-only">Instagram</span>
                 <svg
@@ -63,29 +61,10 @@ function Footer() {
 
             <li>
               <a
-                href="#"
+                href="https://github.com/utsavpatel562"
                 rel="noreferrer"
                 target="_blank"
-                className="text-gray-700 transition hover:text-gray-700/75"
-              >
-                <span className="sr-only">Twitter</span>
-                <svg
-                  className="size-6"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                </svg>
-              </a>
-            </li>
-
-            <li>
-              <a
-                href="#"
-                rel="noreferrer"
-                target="_blank"
-                className="text-gray-700 transition hover:text-gray-700/75"
+                className="text-gray-400 transition hover:text-gray-400/75"
               >
                 <span className="sr-only">GitHub</span>
                 <svg
