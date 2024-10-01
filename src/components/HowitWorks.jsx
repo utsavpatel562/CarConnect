491c47e01c342ba79b5a5cf0e5ca7f87cd26e3c2
utsavpatel562 <utsavpatel562@gmail.com>
2024-10-01 13:49:58 -0400

    New Updated Code

--- src/components/HowitWorks.jsx
+++ src/components/HowitWorks.jsx
@@ -1,4 +1,10 @@
-import { SignIn, SignInButton } from "@clerk/clerk-react";
+import { SignInButton } from "@clerk/clerk-react";
+import { TbLockCog } from "react-icons/tb";
+import { FaSearchengin } from "react-icons/fa";
+import { LuMessagesSquare } from "react-icons/lu";
+import { FaHandHoldingDollar } from "react-icons/fa6";
+import { PiChartDonutDuotone } from "react-icons/pi";
+import { AiOutlineDeploymentUnit } from "react-icons/ai";
 import React from "react";
 
 function HowitWorks() {
@@ -6,17 +12,19 @@ function HowitWorks() {
     <>
       <section className="flex justify-center items-center min-h-screen bg-slate-950">
         <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-          <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
+          <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-0">
             <div className="mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
               <h2 className="text-3xl font-bold sm:text-4xl text-gray-200">
                 HOW IT WORKS
               </h2>
 
-              <p className="mt-4 text-gray-400">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-                vero aliquid sint distinctio iure ipsum cupiditate? Quis, odit
-                assumenda? Deleniti quasi inventore, libero reiciendis minima
-                aliquid tempora. Obcaecati, autem.
+              <p className="mt-4 text-gray-400 text-justify">
+                CarConnect is a cutting-edge marketplace that redefines the way
+                people buy and sell cars, addressing common pain points in the
+                automotive transaction process. Our mission is to create a
+                seamless, efficient, and engaging platform that connects buyers
+                and sellers, fostering trust and transparency in every
+                transaction.
               </p>
 
               <SignInButton className="bg-green-700 hover:bg-green-600 p-2 my-6 w-[180px] h-12 rounded-sm font-bold text-gray-300">
@@ -26,38 +34,95 @@ function HowitWorks() {
 
             <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
               {/* Icons grid */}
-              {[...Array(6)].map((_, index) => (
-                <a
-                  key={index}
-                  className="block rounded-xl border border-green-100 p-4 shadow-sm hover:border-green-400 hover:ring-1 hover:ring-green-400 focus:outline-none focus:ring"
-                  href="#"
-                >
-                  <span className="inline-block rounded-lg bg-gray-50 p-3">
-                    <svg
-                      className="size-6"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                      <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                      ></path>
-                    </svg>
-                  </span>
+              <a
+                className="block rounded-xl border border-green-100 p-4 shadow-sm hover:border-green-400 hover:ring-1 hover:ring-green-400 focus:outline-none focus:ring"
+                href="#"
+              >
+                <span className="inline-block rounded-lg bg-gray-50 p-3">
+                  <TbLockCog />
+                </span>
 
-                  <h2 className="mt-2 font-bold">Accountant</h2>
+                <h2 className="mt-2 font-bold">
+                  User Registration & Authentication
+                </h2>
 
-                  <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-                    Lorem ipsum dolor sit amet consectetur.
-                  </p>
-                </a>
-              ))}
+                <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-300">
+                  Secure user auth, ensures that all transactions are protected
+                </p>
+              </a>
+              <a
+                className="block rounded-xl border border-green-100 p-4 shadow-sm hover:border-green-400 hover:ring-1 hover:ring-green-400 focus:outline-none focus:ring"
+                href="#"
+              >
+                <span className="inline-block rounded-lg bg-gray-50 p-3">
+                  <FaSearchengin />
+                </span>
+
+                <h2 className="mt-2 font-bold">Browsing & Searching Cars</h2>
+
+                <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-300">
+                  Advanced search, allow users to effortlessly find cars.
+                </p>
+              </a>
+              <a
+                className="block rounded-xl border border-green-100 p-4 shadow-sm hover:border-green-400 hover:ring-1 hover:ring-green-400 focus:outline-none focus:ring"
+                href="#"
+              >
+                <span className="inline-block rounded-lg bg-gray-50 p-3">
+                  <LuMessagesSquare />
+                </span>
+
+                <h2 className="mt-2 font-bold">Real-Time Messaging</h2>
+
+                <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                  Sellers can create detailed listings to showcase their
+                  vehicles.
+                </p>
+              </a>
+              <a
+                className="block rounded-xl border border-green-100 p-4 shadow-sm hover:border-green-400 hover:ring-1 hover:ring-green-400 focus:outline-none focus:ring"
+                href="#"
+              >
+                <span className="inline-block rounded-lg bg-gray-50 p-3">
+                  <FaHandHoldingDollar />
+                </span>
+
+                <h2 className="mt-2 font-bold">
+                  Data Management & API Integration
+                </h2>
+
+                <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                  Efficient data management and API integrations.
+                </p>
+              </a>
+              <a
+                className="block rounded-xl border border-green-100 p-4 shadow-sm hover:border-green-400 hover:ring-1 hover:ring-green-400 focus:outline-none focus:ring"
+                href="#"
+              >
+                <span className="inline-block rounded-lg bg-gray-50 p-3">
+                  <PiChartDonutDuotone />
+                </span>
+
+                <h2 className="mt-2 font-bold">Deployment & Hosting</h2>
+
+                <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                  Fast and reliable deployment on Vercel
+                </p>
+              </a>
+              <a
+                className="block rounded-xl border border-green-100 p-4 shadow-sm hover:border-green-400 hover:ring-1 hover:ring-green-400 focus:outline-none focus:ring"
+                href="#"
+              >
+                <span className="inline-block rounded-lg bg-gray-50 p-3">
+                  <AiOutlineDeploymentUnit />
+                </span>
+
+                <h2 className="mt-2 font-bold">Accountant</h2>
+
+                <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                  Lorem ipsum dolor sit amet consectetur.
+                </p>
+              </a>
             </div>
           </div>
         </div>
