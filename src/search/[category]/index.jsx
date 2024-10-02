62abad8c0b79782545e5a6316b744f86595c8816
utsavpatel562 <utsavpatel562@gmail.com>
2024-10-02 12:58:21 -0400

    New Updated Code

--- src/search/[category]/index.jsx
+++ src/search/[category]/index.jsx
@@ -1,8 +1,31 @@
 import Header from "@/components/Header";
 import Search from "@/components/Search";
-import React from "react";
+import { db } from "../../../configs";
+import { CarImages, CarListing } from "../../../configs/schema";
+import { eq } from "drizzle-orm";
+import React, { useEffect } from "react";
+import { useParams } from "react-router";
+import Services from "@/Shared/Services";
 
 function SearchByCategory() {
+  const { category } = useParams();
+  console.log(category);
+
+  useEffect(() => {
+    GetCarList();
+  }, []);
+
+  const GetCarList = async () => {
+    const result = await db
+      .select()
+      .from(CarListing)
+      .innerJoin(CarImages, eq(CarListing.id, CarImages.carListingId))
+      .where(eq(CarListing.category, category));
+
+    const resp = Services.FormResult(result);
+    console.log(resp);
+  };
+
   return (
     <>
       <div>
@@ -15,6 +38,9 @@ function SearchByCategory() {
           }}
         >
           <Search />
+        </div>
+        <div>
+          <h2 className="font-bold text-4xl p-10 md:px-20">{category}</h2>
         </div>
       </div>
     </>
