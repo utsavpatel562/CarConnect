63744947004a15dadf613886c1447a1d906ba63a
utsavpatel562 <utsavpatel562@gmail.com>
2024-10-02 21:06:35 -0400

    New Updated Code

--- src/search/index.jsx
+++ src/search/index.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
+import Services from "@/Shared/Services";
+import { db } from "../../configs";
+import { CarImages, CarListing } from "../../configs/schema";
+import { eq } from "drizzle-orm";
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function SearchByOptions() {
-  return <div>Search By Options</div>;
+  const [searchParam] = useSearchParams();
+  const condition = searchParam.get("cars");
+  const make = searchParam.get("make");
+  const price = searchParam.get("price");
+
+  console.log(condition, make, price);
+
+  useEffect(() => {
+    GetCarList();
+  }, []);
+
+  const GetCarList = async () => {
+    const result = await db
+      .select()
+      .from(CarListing)
+      .innerJoin(CarImages, eq(CarListing.id, CarImages.carListingId))
+      .where(condition != undefined && eq(CarListing.condition, condition))
+      .where(make != undefined && eq(CarListing.make, make));
+
+    const resp = Services.FormResult(result);
+    console.log(resp);
+  };
+
+  return (
+    <>
+      <div>SeachByOptions</div>
+    </>
+  );
 }
 
 export default SearchByOptions;
