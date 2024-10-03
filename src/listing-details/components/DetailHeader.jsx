a277d3524e09085ce74a8ce66bd8c227f7c4c6a7
utsavpatel562 <utsavpatel562@gmail.com>
2024-10-03 15:15:59 -0400

    New Updated Code

--- src/listing-details/components/DetailHeader.jsx
+++ src/listing-details/components/DetailHeader.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { RxCalendar } from "react-icons/rx";
+import { SlSpeedometer } from "react-icons/sl";
+import { GiGearStickPattern } from "react-icons/gi";
+import { LuFuel } from "react-icons/lu";
 function DetailHeader({ carDetail }) {
   return (
     <>
       <h2 className="font-bold text-3xl">{carDetail?.listingTitle}</h2>
-      <p className="text-sm">{carDetail?.tagline}</p>
-      <div className="flex gap-2 mt-2">
-        <div className="flex gap-2 items-center bg-green-50 rounded-full p-2 px-3">
-          <RxCalendar className="h-6 w-6 text-primary" />
+      <p className="text-md">{carDetail?.tagline}</p>
+      <div className="flex gap-2 mt-3">
+        <div className="flex gap-2 items-center bg-green-50 rounded-full p-2 px-3 w-[110px] justify-center shadow-md">
+          <RxCalendar className="h-5 w-5 text-primary" />
           <h2 className="text-primary text-md">{carDetail?.year}</h2>
         </div>
-        <div className="flex gap-2 items-center bg-green-50 rounded-full p-2 px-3">
-          <RxCalendar className="h-6 w-6 text-primary" />
-          <h2 className="text-primary text-md">{carDetail?.year}</h2>
+        <div className="flex gap-2 items-center bg-green-50 rounded-full p-2 px-3 w-[110px] justify-center shadow-md">
+          <SlSpeedometer className="h-5 w-5 text-primary" />
+          <h2 className="text-primary text-md">{carDetail?.mileage}</h2>
         </div>
-        <div className="flex gap-2 items-center bg-green-50 rounded-full p-2 px-3">
-          <RxCalendar className="h-6 w-6 text-primary" />
-          <h2 className="text-primary text-md">{carDetail?.year}</h2>
+        <div className="flex gap-2 items-center bg-green-50 rounded-full p-2 px-3 w-[110px] justify-center shadow-md">
+          <GiGearStickPattern className="h-5 w-5 text-primary" />
+          <h2 className="text-primary text-md">{carDetail?.transmission}</h2>
+        </div>
+        <div className="flex gap-2 items-center bg-green-50 rounded-full p-2 px-3 w-[110px] justify-center shadow-md">
+          <LuFuel className="h-5 w-5 text-primary" />
+          <h2 className="text-primary text-md">{carDetail?.fuelType}</h2>
         </div>
       </div>
     </>
