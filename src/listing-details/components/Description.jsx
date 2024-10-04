78e19e1d3c1f111628c1181478dae3a10d1e807e
utsavpatel562 <utsavpatel562@gmail.com>
2024-10-03 22:09:42 -0400

    New Updated Code

--- src/listing-details/components/Description.jsx
+++ src/listing-details/components/Description.jsx
@@ -5,7 +5,7 @@ function Description({ carDetail }) {
     <>
       <div>
         {carDetail?.listingDescription ? (
-          <div className="p-5 rounded-xl bg-white shadow-md mt-6 border">
+          <div className="p-8 rounded-xl bg-white shadow-md mt-6 border">
             <h2 className="my-2 font-medium text-2xl">Description</h2>
             <p>{carDetail?.listingDescription}</p>
           </div>
--- src/listing-details/components/Features.jsx
+++ src/listing-details/components/Features.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { FaCheck } from "react-icons/fa6";
 
-function Features({ features }) {
+function Features({ features = {} }) {
+  // Add a default value for features
   return (
     <>
       <div className="mt-6">
-        <div className="p-5 bg-white rounded-xl border shadow-md">
+        <div className="p-8 bg-white rounded-xl border shadow-md">
           <h2 className="font-medium text-2xl">Features</h2>
-          <div>
-            {Object.entries(features).map(([features, value]) => (
-              <div>
-                {features} - {value}
+          <div className="mt-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7 ">
+            {Object.entries(features).map(([feature, value], index) => (
+              <div key={index} className="flex gap-2 items-center">
+                {" "}
+                {/* Added key prop */}
+                <FaCheck className="text-lg p-1 rounded-full bg-green-100 text-primary" />
+                <h2>{feature}</h2>
               </div>
             ))}
           </div>
