89da16aa096748afb66c752bfe594fe480415ac6
utsavpatel562 <utsavpatel562@gmail.com>
2024-10-06 12:48:09 -0400

    New Updated Code

--- src/profile/components/Inbox.jsx
+++ src/profile/components/Inbox.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { App as SendbirdApp, SendBirdProvider } from "@sendbird/uikit-react";
 import "@sendbird/uikit-react/dist/index.css";
+import { useUser } from "@clerk/clerk-react";
 
 function Inbox() {
+  const { user } = useUser();
+  const [userId, setUserId] = useState();
+  useEffect(() => {
+    if (user) {
+      const id = (user.primaryEmailAddress?.emailAddress).split("@")[0];
+      setUserId(userId);
+    }
+  }, []);
   return (
     <>
       <div>
         <div style={{ width: "100vw", height: "100vh" }}>
           <SendBirdProvider
+            userId={userId}
+            nickname={user?.fullName}
+            profileUrl={user?.imageUrl}
+            allowProfileEdit={true}
             appId={import.meta.env.VITE_SENDBIRD_APP_ID}
           ></SendBirdProvider>
-          <SendbirdApp
-            // You can find your Sendbird application ID on the Sendbird dashboard.
-            appId={import.meta.env.VITE_SENDBIRD_APP_ID}
-            // Specify the user ID you've created on the dashboard.
-            // Or you can create a user by specifying a unique userId.
-            userId={"USER_ID"}
-          />
         </div>
       </div>
     </>
